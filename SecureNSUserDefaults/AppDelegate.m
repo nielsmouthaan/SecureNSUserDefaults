d007e0b65a7e22a2a9eab14e01083a259d2fb7c6
Bruno R. Nunes <brnunes@gmail.com>
2016-03-15 16:25:33 -0700

    Add view controller to window in app delegate

--- SecureNSUserDefaults/AppDelegate.m
+++ SecureNSUserDefaults/AppDelegate.m
@@ -15,6 +15,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    [self.window setRootViewController:[UIViewController new]];
     [self.window makeKeyAndVisible];
     return YES;
 }
